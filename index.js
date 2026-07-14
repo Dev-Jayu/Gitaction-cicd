10987a38b78b4696f76972c57f55976590f88bba
Jayesh Gavale <jayeshgavale02@pearlautomation.in>
2026-07-14 15:26:54 +0530

    Setup automatic deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello CI/CD d");
+    res.send("Hello CI/CD jayesh");
 });
 
 module.exports = app;
