5b6c8e4fd2bb6d3facef0f853d5092bd7ab1718f
Jayesh Gavale <jayeshgavale02@pearlautomation.in>
2026-07-14 15:56:56 +0530

    new message form yogesh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello sagar its working ");
+    res.send("Hello sagar its now ");
 });
 
 module.exports = app;
